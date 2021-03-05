1a45ed8a8ecb06fde4a8a0c78869d01a86ba4d8d
Xavi-PL <xavipl.mvk25@gmail.com>
2021-03-05 15:59:22 +0100

    added search filter

--- www/js/init.js
+++ www/js/init.js
@@ -21,7 +21,28 @@ function onDeviceReady() {
       });
 
       $('input#etSearch').keyup(function(){
-        console.log($('input#etSearch').val());
+
+        $.ajax({
+          method: "GET",
+          url: "https://musicbrainz.org/ws/2/artist?query=" + $('input#etSearch').val(),
+          dataType: "json",   // necessitem això pq ens retorni un objecte JSON
+        }).done(function (msg) {
+
+          var $results = $('ul#results');
+          $results.empty();
+
+          for(var item in msg.artists) {
+
+            $results.append('<li class="collection-item">' + msg.artists[item].name + '</li>')
+            console.log(msg.artists[item]);
+            // aquí caldría fer mes coses, of course...
+            // ...
+          };
+        }).fail(function () {
+          alert("ERROR");
+        });
+
+        // console.log($('input#etSearch').val());
       })
 
       $('.fixed-action-btn').floatingActionButton();
